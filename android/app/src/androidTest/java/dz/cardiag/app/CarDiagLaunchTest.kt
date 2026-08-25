da994005a554b4035bee10b5f3f92b625c73219f
Tarek76578 <168303910+Tarek76578@users.noreply.github.com>
2026-08-25 19:11:53 +0100

    test: strengthen CarDiag runtime smoke test

--- android/app/src/androidTest/java/dz/cardiag/app/CarDiagLaunchTest.kt
+++ android/app/src/androidTest/java/dz/cardiag/app/CarDiagLaunchTest.kt
@@ -2,6 +2,8 @@ package dz.cardiag.app
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -13,6 +15,9 @@ class CarDiagLaunchTest {
 
     @Test
     fun launchActivity_doesNotCrash() {
-        check(!activityRule.activity.isFinishing)
+        val activity = activityRule.activity
+        assertFalse("Launcher Activity is finishing", activity.isFinishing)
+        assertFalse("Launcher Activity is destroyed", activity.isDestroyed)
+        assertTrue("Compose content was not attached", activity.findViewById<android.view.ViewGroup>(android.R.id.content).childCount > 0)
     }
 }
