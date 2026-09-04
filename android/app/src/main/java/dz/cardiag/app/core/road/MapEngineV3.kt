fec7b77603f99f1ed3a2293dfda67b32fb4459fb
Tarek76578 <168303910+Tarek76578@users.noreply.github.com>
2026-09-04 15:42:53 +0100

    fix(map): correct Ktor timeout import

--- android/app/src/main/java/dz/cardiag/app/core/road/MapEngineV3.kt
+++ android/app/src/main/java/dz/cardiag/app/core/road/MapEngineV3.kt
@@ -2,7 +2,7 @@ package dz.cardiag.app.core.road
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
-import io.ktor.client.plugins.timeout.HttpTimeout
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.parameter
